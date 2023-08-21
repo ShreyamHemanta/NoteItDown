6a542113669a61685e159990037bd4945d56dcec
Shreyam Hemanta <111384933+ShreyamHemanta@users.noreply.github.com>
2023-08-21 22:31:49 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Header from "Header.jsx";
-import Footer from "Footer.jsx";
-import Note from "Note.jsx";
-import CreateArea from "CreateArea.jsx";
+import Header from "Header";
+import Footer from "Footer";
+import Note from "Note";
+import CreateArea from "CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
