6f7fc141125cbe11585e62028b7457f275775dec
Shreyam Hemanta <111384933+ShreyamHemanta@users.noreply.github.com>
2023-08-22 22:43:01 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
-import Header from "Header.jsx";
-import Footer from "Footer.jsx";
-import Note from "Note.jsx";
-import CreateArea from "CreateArea.jsx";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import CreateArea from "./CreateArea.jsx";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
